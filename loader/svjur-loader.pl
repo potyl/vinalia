87d4dded8f3a63a39451241215dcb3a4bf910802
martin.rusko <martin.rusko@9fcaeb2c-1fba-11de-9909-f37ff7dbbc12>
2009-04-03 10:49:20 +0000

    Processing wines, dummy functions for tables added

--- loader/svjur-loader.pl
+++ loader/svjur-loader.pl
@@ -18,8 +18,59 @@ if (my $inputfile = $ARGV[0] ) {
 
 	close($inputfile);
 }
+#print Dumper($wines);
 
-print Dumper($wines);
+
+my $tbl_wines = {};
+my $tbl_varieties = {};
+my $tbl_producers = {};
+my $tbl_colors = {};
+my $tbl_categories = {};
+
+foreach my $wine ( @{$wines} ) {
+
+	my $vid = get_variety_id($wine->{variety}, $wine->{category});
+	my $pid = get_producer_id($wine->{producer});
+	my $cid = get_category_id($wine->{category});
+	my $aid = get_attribute_id($wine->{attribute});
+
+	#$wine->{variety_id} = $vid;
+	#$wine->{producer_id} = $pid;
+	#$wine->{category_id} = $cid;
+	#$wine->{attribute_id} = $aid;
+
+	my $sql = "INSERT INTO wines (wine_id,producer_id,variety_id,attribute_id,category_id,year) ";
+	$sql .= "VALUES (" . $wine->{wine_id} . ",$pid,$vid,$aid,$cid," . $wine->{year} . ");\n";
+
+	$wine->{sql} = $sql; 
+
+	$tbl_wines->{$wine->{wine_id}} = $wine;
+}
+
+print Dumper($tbl_wines);
+
+
+## SUB ROUTINES ##
+
+sub get_variety_id {
+	
+	return 1;
+}
+
+sub get_producer_id {
+	
+	return 1;
+}
+
+sub get_category_id {
+	
+	return 1;
+}
+
+sub get_attribute_id {
+	
+	return 1;
+}
 
 # 
 # Parse CSV file
